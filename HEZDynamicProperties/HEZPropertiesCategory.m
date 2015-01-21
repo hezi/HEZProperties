b7069be6839874ebd0d551ff389fdb5cd7851c6d
Hezi Cohen <hezi.co@gmail.com>
2015-01-21 19:18:27 +0200

    added a way to get all collection names in a cat

--- HEZDynamicProperties/HEZPropertiesCategory.m
+++ HEZDynamicProperties/HEZPropertiesCategory.m
@@ -7,33 +7,37 @@
 #import "HEZPropertiesCategory.h"
 #import "HEZPropertiesCollection.h"
 
-@implementation HEZPropertiesCategory {
+@implementation HEZPropertiesCategory
+{
     NSMutableDictionary *_collections;
 }
 
-+ (instancetype)categoryFromDictionary:(NSDictionary*)aDictionary
++ (instancetype)categoryFromDictionary:(NSDictionary *)aDictionary
 {
     return [[self alloc] initWithDictionary:aDictionary];
 }
 
-- (instancetype)initWithDictionary:(NSDictionary*)aDictionary
+- (instancetype)initWithDictionary:(NSDictionary *)aDictionary
 {
     self = [super init];
-    if (self) {
+    if(self)
+    {
         _collections = [[NSMutableDictionary alloc] init];
         [self loadFromDictionary:aDictionary];
     }
     return self;
 }
 
-- (void)loadFromDictionary:(NSDictionary*)aDictionary
+- (void)loadFromDictionary:(NSDictionary *)aDictionary
 {
     for(NSString *collectionName in [aDictionary allKeys])
     {
         HEZPropertiesCollection *collection = _collections[collectionName] ?: [HEZPropertiesCollection collectionFromDictionary:aDictionary[collectionName]];
-        
-        if(!_collections[collectionName]) {
-            if(collection){
+
+        if(!_collections[collectionName])
+        {
+            if(collection)
+            {
                 [collection setName:collectionName];
                 [_collections setObject:collection forKey:collectionName];
             }
@@ -43,7 +47,12 @@
     }
 }
 
-- (HEZPropertiesCollection*)collectionWithName:(NSString*)name
+- (NSArray *)allCollectionNames
+{
+    return [_collections allKeys];
+}
+
+- (HEZPropertiesCollection *)collectionWithName:(NSString *)name
 {
     return _collections[name];
 }
